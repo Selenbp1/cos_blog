9b418fcf8803bdc6cb6dd7814eea6634b3d0c957
Selenbp1 <akfkdhwk@gmail.com>
2022-03-16 01:57:30 +0900

    http 테스트

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package com.cos.blog;
 
+import com.cos.blog.test.HttpControllerTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+        SpringApplication.run(HttpControllerTest.class, args);
     }
 
 }
---
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,44 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.*;
+
+//사용자가 요청 -> 응답(HTML파일)
+//@Controller
+
+//사용자가 요청 -> 응답(Data)
+@RestController
+public class HttpControllerTest {
+    //http://localhost:8080/http/get (select)
+    @GetMapping("/http/get")
+    public String getTest(Member m){    //id=1&username=cos&password=1234&email=cos@gmail.com
+        return "get 요청 : "+ m.getId() +", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    }
+
+    //http://localhost:8080/http/post (insert)
+    /*@PostMapping("/http/post")
+    public String postTest(Member m){
+        return "post 요청 : "+ m.getId() +", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    }*/
+
+/*    @PostMapping("/http/post")
+    public String postTest(@RequestBody String text){   //text/plain
+        return "post 요청 : "+text;
+    }*/
+
+    @PostMapping("/http/post")  //MessageConverter(스프링부트)
+    public String postTest(@RequestBody Member m){   //application/json
+        return "post 요청 : "+ m.getId() +", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
+    }
+
+    //http://localhost:8080/http/put (update)
+    @PutMapping("/http/put")
+    public String putTest(){
+        return "put 요청";
+    }
+
+    //http://localhost:8080/http/delete (delete)
+    @DeleteMapping("/http/delete")
+    public String deleteTest(){
+        return "delete 요청";
+    }
+}
