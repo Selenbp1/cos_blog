96b14d25740e7ada3174ee9333e4da8acf2a3bd1
Selenbp1 <akfkdhwk@gmail.com>
2022-03-16 19:14:42 +0900

    전체 select 및 페이징 테스트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -4,11 +4,16 @@ import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 //html 파일이 아니라 data를 리턴해주는 controller = RestController
@@ -17,6 +22,21 @@ public class DummyControllerTest {
 
     @Autowired//의존성 주입(DI)
     private UserRepository userRepository;
+
+    //http://localhost:8000/blog/dummy/user
+    @GetMapping("/dummy/users")
+    public List<User> list(){
+        return  userRepository.findAll();
+    }
+
+    //한페이지당 2건에 데이터를 리턴받아 볼 예정
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size=2,  sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<User> pagingUser= userRepository.findAll(pageable);
+
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
 
     //{id}주소로 파라미터를 전달받을 수 잇음
     //http://localhost:8000/blog/dummy/user/3
@@ -45,7 +65,7 @@ public class DummyControllerTest {
         //변환(웹브라우저가 이해할 수 있는 데이터) -> json (Gson 라이브러리)
         // 스프링부트 = MessageConverter라는 애가 응답시에 자동 작동
         //만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
-        //user 오브젝트를 json으로 변환해서 브라우저에게 던져줌 
+        //user 오브젝트를 json으로 변환해서 브라우저에게 던져줌
         return user;
     }
 
